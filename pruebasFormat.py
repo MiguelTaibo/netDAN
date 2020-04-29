e91e83aa926f7572415daeea50a5d980688ba4c3
MiguelTaibo <m.taibo@alumnos.upm.es>
2020-04-29 19:13:49 +0200

    version 1.0

---
+++ pruebasFormat.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import shutil
+
+dataroot = "/mnt/pgth04b/Data_Miguel/AutoEncoders/datasets/AFFECTNET"
+
+Image = []
+Landmark = []
+Emotion = []
+
+columns = ['subDirectory_filePath','face_x','face_y','face_width','face_height','facial_landmarks','expression','valence','arousal']
+
+data = pd.read_csv('training.csv');
+
+for landmarks in data['facial_landmarks']:
+    try:
+        s = landmarks.split(';')
+        lm = []
+        for str in s:
+            lm.append(float(str))
+        Landmark.append(lm)
+    except:
+        print("ERROR")
+        pass
+
+for filename in data['subDirectory_filePath']:
+    try:
+        shutil.copy(dataroot+'/'+filename,'datasets/'+filename)
+    except:
+        print(dataroot+'/'+filename, "  ", 'datasets/'+filename)
+
+Landmark = np.array(Landmark)
+#import pdb
+#pdb.set_trace()
+print(Landmark)
