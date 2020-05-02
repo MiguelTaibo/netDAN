983659c0c33c9863c8986389cce62182b046d9f1
MiguelTaibo <m.taibo@alumnos.upm.es>
2020-05-02 14:02:24 +0200

    crear labels con args

--- createLabels.py
+++ createLabels.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import argparse
 import cv2
 import shutil, os
 from PIL import Image
@@ -14,10 +15,20 @@ def plot_img(img, lms, i, f):
 
 
 if __name__ == "__main__":
-    quiet = False
-    dataroot = "datasets/"
-    data = pd.read_csv('training.csv')
-    out_path = "results/"
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--quiet", action='store_true')
+    ap.add_argument("--dataroot", type=str, default='datasets/')
+    ap.add_argument("--csv_file", type=str, default='training.csv')
+    ap.add_argument("--out_path", type=str, default='results/')
+    ap.add_argument("--longSize", type=int, default=128)
+
+    args = vars(ap.parse_args())
+
+    quiet = args['quiet']
+    dataroot = args['dataroot']
+    data = pd.read_csv(args['csv_file'])
+    out_path = args['out_path']
+    IMG_SIZE = args['longSize']
 
     columns = ['subDirectory_filePath', 'face_x', 'face_y', 'face_width', 'face_height', 'facial_landmarks', 'expression', 'valence', 'arousal']
     data = pd.read_csv('training.csv')
@@ -25,9 +36,7 @@ if __name__ == "__main__":
     for index, row in data.iterrows():
         ### LOAD IMAGE DATA
         img_ori = cv2.imread(dataroot+row['subDirectory_filePath'] )
-        print(img_ori.shape)
         (width, height, _) = img_ori.shape
-        print(width, height)
 
         ### LOAD LANDMARKS
         landmarks = []
@@ -48,12 +57,12 @@ if __name__ == "__main__":
         # exit()
 
         for i in range(68):
-            landmarks[i, 0] = min(landmarks[i, 0] / width * 224,223)
-            landmarks[i, 1] = min(landmarks[i, 1] / height * 224,233)
+            landmarks[i, 0] = min(landmarks[i, 0] / width * IMG_SIZE,223)
+            landmarks[i, 1] = min(landmarks[i, 1] / height * IMG_SIZE,233)
 
 
         ## CREATE LABEL
-        background = np.zeros((224,224))
+        background = np.zeros((IMG_SIZE,IMG_SIZE))
         colors = []
         [colors.append((255, 255, 255)) for roi in range(8)]
         output = face_utils.visualize_facial_landmarks(background, landmarks.astype('int64'), colors=colors, alpha=1)
